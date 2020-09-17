7e1b4a1bf46fe14224f83eaf4538a4f15d3c99b0
Екатерина Пожидаева <kateworks@yandex.ru>
2020-09-17 10:47:09 +0300

    Updates

---
+++ precache-manifest.ed8f0ce3c7cad4140ccb6f28404302ff.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dfae87bb5798bce63a18034068429bf",
+    "url": "/mesto-react/index.html"
+  },
+  {
+    "revision": "fa059c1cc32e56ff6dc8",
+    "url": "/mesto-react/static/css/main.d5d20214.chunk.css"
+  },
+  {
+    "revision": "f92f58c917a08ed4a68f",
+    "url": "/mesto-react/static/js/2.d02da96c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mesto-react/static/js/2.d02da96c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa059c1cc32e56ff6dc8",
+    "url": "/mesto-react/static/js/main.1374f7f7.chunk.js"
+  },
+  {
+    "revision": "6e04319a44e5e6859875",
+    "url": "/mesto-react/static/js/runtime-main.36fb0e45.js"
+  },
+  {
+    "revision": "438d040613f4ab457a2bd158608f9068",
+    "url": "/mesto-react/static/media/Inter-Black.438d0406.woff"
+  },
+  {
+    "revision": "d2f835f3c9535d55d27e63f4b3da5cfb",
+    "url": "/mesto-react/static/media/Inter-Black.d2f835f3.woff2"
+  },
+  {
+    "revision": "0ed54f43055ff8b96c54dd4a8c30decf",
+    "url": "/mesto-react/static/media/Inter-Bold.0ed54f43.woff"
+  },
+  {
+    "revision": "53dcdee153bfdb439455242d611e7afd",
+    "url": "/mesto-react/static/media/Inter-Bold.53dcdee1.woff2"
+  },
+  {
+    "revision": "35ee0c124d0e19a342243bcaa1ae2f48",
+    "url": "/mesto-react/static/media/Inter-Medium.35ee0c12.woff2"
+  },
+  {
+    "revision": "85f4b8e903fbd1d2c98a67dd0dca42e7",
+    "url": "/mesto-react/static/media/Inter-Medium.85f4b8e9.woff"
+  },
+  {
+    "revision": "58b22f90caa86e59747ab7020e890857",
+    "url": "/mesto-react/static/media/Inter-Regular.58b22f90.woff2"
+  },
+  {
+    "revision": "a5e1eecae07839e1412d26ecac573fe1",
+    "url": "/mesto-react/static/media/Inter-Regular.a5e1eeca.woff"
+  },
+  {
+    "revision": "9c3b03ae735a14dc757b4d25c0ba5fb3",
+    "url": "/mesto-react/static/media/card-btn-delete.9c3b03ae.svg"
+  },
+  {
+    "revision": "695b001f43592c21b7c52fc77a74a754",
+    "url": "/mesto-react/static/media/card-btn-like-clicked.695b001f.svg"
+  },
+  {
+    "revision": "6c74d9b53442a247e20750f5de0be939",
+    "url": "/mesto-react/static/media/card-btn-like.6c74d9b5.svg"
+  },
+  {
+    "revision": "855a8c9865e79463f31e74f9abcb4438",
+    "url": "/mesto-react/static/media/header-logo.855a8c98.svg"
+  },
+  {
+    "revision": "138fc0be11174e50ff645529807dbe55",
+    "url": "/mesto-react/static/media/photo-grid-aleksandria.138fc0be.jpg"
+  },
+  {
+    "revision": "02fa6c01df4e3c14273057f9a7be0ddb",
+    "url": "/mesto-react/static/media/photo-grid-oranienbaum-1.02fa6c01.jpg"
+  },
+  {
+    "revision": "e1f972ed56bed40ef0ad984a18815375",
+    "url": "/mesto-react/static/media/photo-grid-peterhof-1.e1f972ed.jpg"
+  },
+  {
+    "revision": "21d5c7ebddee3c7147fe6f64d1ad02f0",
+    "url": "/mesto-react/static/media/photo-grid-peterhof-3.21d5c7eb.jpg"
+  },
+  {
+    "revision": "bf26b1954a96891636a4bc870505d14e",
+    "url": "/mesto-react/static/media/photo-grid-peterhof-4.bf26b195.jpg"
+  },
+  {
+    "revision": "7b0982f38e23d7abe8b73098234bcdbe",
+    "url": "/mesto-react/static/media/photo-grid-peterhof.7b0982f3.jpg"
+  },
+  {
+    "revision": "c2d0901f14de58acae456bda23f80e8a",
+    "url": "/mesto-react/static/media/popup-btn-close.c2d0901f.svg"
+  },
+  {
+    "revision": "a60c8e94ab1bc92a65b2f1582ba36003",
+    "url": "/mesto-react/static/media/profile-avatar.a60c8e94.jpg"
+  },
+  {
+    "revision": "3e4d607fed81a55fc99c6d87560bc2fa",
+    "url": "/mesto-react/static/media/profile-btn-add.3e4d607f.svg"
+  },
+  {
+    "revision": "41b73632a217006ed5f9258b81a92adf",
+    "url": "/mesto-react/static/media/profile-btn-edit.41b73632.svg"
+  }
+]);
--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = () => (
-  <footer className="footer">
-    <p className="footer__copyright">© 2020 Mesto Russia</p>
-  </footer>
-);
-
-export default Footer;
