20dbba5e8bdd1cc3d872fc7ae733e3b45ea79a72
Екатерина Пожидаева <kateworks@yandex.ru>
2020-09-25 18:22:25 +0300

    feat: PopupWithForm добавить закрытие при нажатии ESC и при клике за пределами формы

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function PopupWithForm(props) {
   const openClass = props.isOpen && 'popup_opened';
 
+  const handleCloseClick = () => {
+    props.onClose();
+  };
+
+  const handleOverlayClick = (evt) => {
+    if (evt.target === evt.currentTarget) handleCloseClick();
+  };
+
+  useEffect(() => {
+    const handleEscPress = (evt) => {
+      if (evt.key === 'Escape') handleCloseClick();
+    };
+    
+    if (props.isOpen) {
+      document.addEventListener('keyup', handleEscPress);
+    } else {
+      document.removeEventListener('keyup', handleEscPress);
+    };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [props.isOpen]);
+
   return(
-    <div className={`popup popup_content_${props.name} ${openClass}`}>
+    <div 
+      className={`popup popup_content_${props.name} ${openClass}`}
+      onClick={handleOverlayClick}
+    >
     <div className="popup__container">
 
       <button 
         type="button" 
         className="popup__btn popup__btn_action_close shaded"
         title="Закрыть форму без сохранения данных"
-        onClick={props.onClose} 
+        onClick={handleCloseClick} 
       />
 
       <form 
